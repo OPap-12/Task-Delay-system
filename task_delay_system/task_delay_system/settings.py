e97aa05b5dcdb29bf56358260e889cf4e942fc7a
OPap-12 <arpit180105@gmail.com>
2026-02-11 17:55:29 +0530

    commit

--- task_delay_system/task_delay_system/settings.py
+++ task_delay_system/task_delay_system/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "tasks" / "static",
+   
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
